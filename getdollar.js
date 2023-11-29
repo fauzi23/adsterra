935a35d63034dab540a8c4d67a0433240fbe50f7
fauzi23 <43736994+fauzi23@users.noreply.github.com>
2023-11-30 05:02:29 +0700

    Create getdollar.js

---
+++ getdollar.js
@@ -0,0 +1,5 @@
+<script language="javascript">
+  setTimeout(function () {
+    window.location.href = "https://www.highcpmrevenuegate.com/nx0mqssqb?key=3b6c383d0fe4dfbd2cebe336916a95f1";
+  }, 3000); // Menunda selama 3 detik (3000 milidetik)
+</script>
